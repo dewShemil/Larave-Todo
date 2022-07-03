e6158132aa114d449703bb5527298299e58b2199
batsdk <shemilkaweesha@gmail.com>
2022-07-03 21:14:20 +0530

    Basic Controllers and Routes Added

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ItemController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +17,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get("/items",[ItemController::class,"index"]);
+Route::prefix("/items")->group(function (){
+    Route::post("/store",itemController::class,"store");
+    Route::put("/{id}",itemController::class,"update");
+    Route::delete("/{id}",itemController::class,"destroy");
 });
